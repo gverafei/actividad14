9de2afe1c4a9dee105b76fb481f22e16ae6aa755
gverafei <gvera@uv.mx>
2023-11-04 21:58:01 -0600

    Add project files.

---
+++ MvcFei/Models/AlumnoCarreraViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcFei.Models
+{
+    public class AlumnoCarreraViewModel
+    {
+        // Lista de alumnos
+        public List<Alumno> Alumnos { get; set; }
+
+        // Lista de carreras para seleccionar
+        public SelectList Carreras { get; set; }
+
+        //Contiene la carrera seleccionada
+        public string? AlumnoCarrera { get; set; }
+
+        //  Contiene el texto que los usuarios escriben en el cuadro de texto de búsqueda
+        public string? SearchString { get; set; }
+    }
+}
